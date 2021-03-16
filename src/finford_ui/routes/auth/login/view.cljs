2f5ffd9a1b09ea3fa4ef8403c42a1fdb12f92e76
Jaret Binford <jaretbinford@gmail.com>
2021-03-16 12:05:12 -0400

    View and package

--- src/finford_ui/routes/auth/login/view.cljs
+++ src/finford_ui/routes/auth/login/view.cljs
@@ -9,7 +9,7 @@
 
 (defn username-input-box []
   [validated-input/text {:id "login-username-input"
-                         :label "Username or Email  "
+                         :label "Username "
                          :route :login
                          :form :login-form
                          :field :username}])
@@ -18,37 +18,38 @@
   (let [password-visible? (<sub [::route-subs/login-form-password-visible?])]
     [validated-input/text {:id "login-password-input"
                            :type (if password-visible? "text" "password")
-                           :label "Password  "
+                           :label "Password "
                            :route :login
                            :form :login-form
                            :field :password}]))
 
 (defn toggle-password-visibility-button []
   (let [password-visible? (<sub [::route-subs/login-form-password-visible?])]
-    [:span#pass-toggle {:on-click #(>dis [::route-events/toggle-password-input-visibility])}
-    (if password-visible? [:img {:src "https://raw.githubusercontent.com/jr0cket/developer-guides/master/clojure/clojure-bank-coin.png"}] [:img {:src "https://raw.githubusercontent.com/jr0cket/developer-guides/master/clojure/clojure-bank-coin.png"}])]))
+    [:a {:on-click #(>dis [::route-events/toggle-password-input-visibility])}
+    (if password-visible? [:img {:src "https://raw.githubusercontent.com/Jaretbinford/finford-ui/master/resources/images/noshow.png" :width "50" :height "25"}] [:img {:src "https://raw.githubusercontent.com/Jaretbinford/finford-ui/75f4ec39e638ed52647f8919e5ee8f79efe2209d/resources/images/unlockshow.png" :width "50" :height "25"}])]))
 
 (defn log-in-button []
-  [:div [:button {:type "submit"
-                  :on-click (fn [evt]
+  [:div [:button#login-button
+         {:type "submit"
+          :on-click (fn [evt]
                               (.preventDefault evt)
                               (>dis [::shared-events/submit-if-form-valid
                                      :login
                                      :login-form
                                      [::route-events/login]]))}
-         "Log In"]])
+         "Log in"]])
 
 (defn template []
   (let [{:keys [username password]} (<sub [::shared-subs/form :login :login-form])]
     [:div#login-wrapper
      [:form
-      [:div
+      [:div#username
        [username-input-box]]
+      [:div#password
+       [password-input-box]]
       [:div
-       [password-input-box]
        [toggle-password-visibility-button]]
-      [log-in-button]]
+       [log-in-button]]
      [:div
-      [:a {:href "#/lost-pass"} "Lost Password"]]
-     [:div
-      [:a {:href "#/register"} "Register"]]]))
+      [:a {:href "#/lost-pass"} "Lost Password "] [:a {:href "#/register"} "Register"]]
+]))
